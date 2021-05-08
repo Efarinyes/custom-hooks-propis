615ce9e9ddbbbf7ab74a9413f3b6756912af6c1e
Efarinyes <efarinyes@gmail.com>
2021-05-08 20:29:22 +0200

    Primer commit

---
+++ useForm.js
@@ -0,0 +1,21 @@
+import { useState } from "react"
+
+
+export const useForm = (initialstate = {}) => {
+
+    const [values, setValues] = useState(initialstate);
+
+    const reset = () => {
+        setValues(initialstate)
+    }
+
+    const handleInputChange = ({ target }) => {
+
+        setValues({
+            ...values,
+            [target.name]: target.value
+        })
+    }
+
+    return [values, handleInputChange, reset];
+}
